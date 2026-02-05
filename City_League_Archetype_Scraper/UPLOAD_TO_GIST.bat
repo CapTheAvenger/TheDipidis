692fc072a7f2e6067a37a2a38a54ca63d553ddb4
CapTheAvenger <haushalterj@me.com>
2026-02-05 09:34:56 +0100

    Clean repo setup: minimal files only

--- City_League_Archetype_Scraper/UPLOAD_TO_GIST.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-python "..\source\upload_to_gist.py" "city_league_archetypes_comparison.html"
-pause
--- Limitless_Online_Scraper/UPLOAD_TO_GIST.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-python "..\source\upload_to_gist.py" "limitless_online_decks_comparison.html"
-pause

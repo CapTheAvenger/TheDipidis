e9a985a1ba861a9dc279324f547d0d5bfa53ddd5
CapTheAvenger <haushalterj@me.com>
2026-03-01 19:57:48 +0100

    Update data files with latest scraper results
    - Updated tournament cards data (cards and overview)
    - Updated city league archetypes comparison and stats
    - Updated current meta card data
    - Updated Japanese cards database
    - Updated all cards scraped pages
    - Updated pokemon sets order

--- data/pokemon_sets_order.js
+++ data/pokemon_sets_order.js
@@ -1,5 +1,5 @@
 // Pokemon TCG Set Order Mapping
-// Auto-generated from Limitless TCG on 2026-03-01 06:51
+// Auto-generated from Limitless TCG on 2026-03-01 09:05
 // Higher number = newer set
 
 const SET_ORDER = {
